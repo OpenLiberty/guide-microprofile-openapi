14179f24d136bebd537d8058e3a1ccdec1ea1d05
Dmitry Kubikov <dmitry.kubikov@ibm.com>
2018-02-27 16:30:43 -0500

    added filtering

--- finish/src/main/java/io/openliberty/guides/filter/InventoryOASFilter.java
+++ finish/src/main/java/io/openliberty/guides/filter/InventoryOASFilter.java
@@ -1,7 +1,28 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
 package io.openliberty.guides.filter;
 
+import java.util.Arrays;
+
+import org.eclipse.microprofile.openapi.OASFactory;
 import org.eclipse.microprofile.openapi.OASFilter;
+import org.eclipse.microprofile.openapi.models.OpenAPI;
+import org.eclipse.microprofile.openapi.models.info.License;
+import org.eclipse.microprofile.openapi.models.info.Info;
 import org.eclipse.microprofile.openapi.models.responses.APIResponse;
+import org.eclipse.microprofile.openapi.models.servers.Server;
+import org.eclipse.microprofile.openapi.models.servers.ServerVariable;
+import org.eclipse.microprofile.openapi.models.servers.ServerVariables;
 
 public class InventoryOASFilter implements OASFilter {
     
@@ -11,6 +32,25 @@ public class InventoryOASFilter implements OASFilter {
             apiResponse.setDescription("Invalid hostname or the system service may not be running on the particular host.");
         }
         return apiResponse;
+    }
+    
+    @Override
+    public void filterOpenAPI(OpenAPI openAPI) {
+        openAPI.setInfo(OASFactory.createObject(Info.class)
+                .title("Inventory App")
+                .version("1.0")
+                .description("Application for storing JVM system properties of various hosts.")
+                .license(OASFactory.createObject(License.class)
+                        .name("Eclipse Public License - v 1.0")
+                        .url("https://www.eclipse.org/legal/epl-v10.html")));
+        
+        openAPI.setServers(Arrays.asList(OASFactory.createObject(Server.class)
+                .url("http://localhost:{port}")
+                .description("Simple Open Liberty.")
+                .variables(OASFactory.createObject(ServerVariables.class)
+                        .addServerVariable("port", OASFactory.createObject(ServerVariable.class)
+                                .description("Server HTTP port.")
+                                .defaultValue("9080")))));
     }
 
 }
