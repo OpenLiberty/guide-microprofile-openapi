b329070c649008ce93258fea416fe7829e24d690
DannyS03 <danny.salman@ryerson.ca>
2019-11-13 14:52:32 -0500

    fixes

--- finish/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
+++ finish/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2018 IBM Corporation and others.
+ * Copyright (c) 2017, 2018, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -60,6 +60,7 @@ public class InventoryResource {
                 // tag::Schema[]
                 schema = @Schema(implementation = Properties.class))) })
                 // end::Schema[]
+    // end::APIResponses[]
     // tag::Operation[]
     @Operation(
         summary = "Get JVM system properties for particular host",
--- finish/src/main/java/io/openliberty/guides/inventory/model/InventoryList.java
+++ finish/src/main/java/io/openliberty/guides/inventory/model/InventoryList.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2018 IBM Corporation and others.
+ * Copyright (c) 2017, 2018, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,8 +13,8 @@
 package io.openliberty.guides.inventory.model;
 
 import java.util.List;
-
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 // tag::InventoryList[]
 @Schema(name="InventoryList", description="POJO that represents the inventory contents.")
 // end::InventoryList[]
