c8b351a96d3d470e7ab519ec6228dd3018501196
Tien-Thanh Le <tienthanh.le@mail.utoronto.ca>
2021-05-11 17:45:35 -0400

    mvn timeout error fix

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -14,7 +14,10 @@ set -euxo pipefail
 #       liberty:create            - Create a Liberty server.
 #       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
 #       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory.
-mvn -q clean package liberty:create liberty:install-feature liberty:deploy
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -q clean package liberty:create liberty:install-feature liberty:deploy
 
 ## Run the tests
 # These commands are separated because if one of the commands fail, the test script will fail and exit.
@@ -24,5 +27,8 @@ mvn -q clean package liberty:create liberty:install-feature liberty:deploy
 #       liberty:stop              - Stop a Liberty server.
 #       failsafe:verify           - Verifies that the integration tests of an application passed.
 mvn liberty:start
-mvn failsafe:integration-test liberty:stop
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    failsafe:integration-test liberty:stop
 mvn failsafe:verify
