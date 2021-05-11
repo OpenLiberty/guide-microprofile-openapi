4da84585ba551db2b0b20fc8f56de362ee52a14d
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-11 11:35:01 -0400

    update versions

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-while getopts t:d:b:u: flag; do
+while getopts t:d: flag; do
     case "${flag}" in
     t) DATE="${OPTARG}" ;;
     d) DRIVER="${OPTARG}" ;;
-    b) BUILD="${OPTARG}" ;;
-    u) DOCKER_USERNAME="${OPTARG}" ;;
+    *) echo "Invalid option";;
     esac
 done
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" pom.xml
 cat pom.xml
-
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" Dockerfile
-cat Dockerfile
 
 ../scripts/testApp.sh
