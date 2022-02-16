88c8b0efc98eaf99495b29d40bcb8a8414543074
t27gupta <tushar.gupta@uwaterloo.ca>
2022-02-16 10:34:33 -0500

    linter fix

--- start/src/main/java/io/openliberty/guides/inventory/InventoryUtils.java
+++ start/src/main/java/io/openliberty/guides/inventory/InventoryUtils.java
@@ -32,11 +32,11 @@ public class InventoryUtils {
             String customUrlString = "http://" + hostname + ":"
                                       + SYSTEM_PORT + "/inventory";
             URL customURL = new URL(customUrlString);
-            SystemClient customRestClient = 
+            SystemClient customRestClient =
                                        RestClientBuilder.newBuilder()
-                                                        .baseUrl(customURL)
-                                                        .register(UnknownUrlExceptionMapper.class)
-                                                        .build(SystemClient.class);
+                                        .baseUrl(customURL)
+                                        .register(UnknownUrlExceptionMapper.class)
+                                        .build(SystemClient.class);
             return customRestClient.getProperties();
         } catch (ProcessingException ex) {
             handleProcessingException(ex);
