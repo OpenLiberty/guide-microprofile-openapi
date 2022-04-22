df589d20f4b08cfe896203fdc2c0f5597fdd39d5
t27gupta <47546702+t27gupta@users.noreply.github.com>
2022-04-22 12:29:52 -0400

    tgupta update (#148)
    * restore to use latest OL
    
    * removed comment from script
    
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -7,8 +7,7 @@ while getopts t:d: flag; do
     esac
 done
 
-#sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" pom.xml
-sed -i "\#<version>22.0.0.2</version>#c<runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl>" pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" pom.xml
 cat pom.xml
 
 ../scripts/testApp.sh
